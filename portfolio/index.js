45be887399bd5d9f3f3cbb7270df5506c0832838
Shubhrima Jana <shubhrijana@gmail.com>
2021-05-30 17:36:59 +0530

    update

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,4 +1,25 @@
 
+/*$(document).ready(function() {
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element 
+        $('.card.block ').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it 
+            if( (bottom_of_window+200) > bottom_of_object ){
+                $(this).animate({'opacity':'1'},500)
+                        .fadeIn("slow");
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});*/
 
 function openProject(evt, cityName) {
   // Declare all variables
