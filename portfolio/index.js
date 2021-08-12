ecfc421413f8426d215d1a6026072e62d7eeb777
Shubhrima Jana <shubhrijana@gmail.com>
2021-08-12 18:15:43 +0530

    loader

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,25 +1,9 @@
-
-/*$(document).ready(function() {
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element 
-        $('.card.block ').each( function(i){
-            
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it 
-            if( (bottom_of_window+200) > bottom_of_object ){
-                $(this).animate({'opacity':'1'},500)
-                        .fadeIn("slow");
-                    
-            }
-            
-        }); 
-    
-    });
-    
-});*/
+window.addEventListener("load", function(){
+    document.querySelector(".preloader").classList.add("opacity-0");
+   setTimeout(function(){
+       document.querySelector(".preloader").style.display="none";
+   },1000)
+});
 
 function openProject(evt, cityName) {
   // Declare all variables
